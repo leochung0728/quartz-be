1e135fb8446bbab85f20d14d08785594582272ae
leochung0728 <leochung0728@gmail.com>
2021-12-10 14:11:41 +0800

    update

--- src/main/java/com/leochung0728/quartz/stock/strategy/EPSMeanCompareStrategy.java
+++ src/main/java/com/leochung0728/quartz/stock/strategy/EPSMeanCompareStrategy.java
@@ -1,6 +1,7 @@
 package com.leochung0728.quartz.stock.strategy;
 
 import com.leochung0728.quartz.enums.SeasonMonth;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 
 import static tech.tablesaw.aggregate.AggregateFunctions.*;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class EPSMeanCompareStrategy implements Strategy {
 
     Table epsData;
@@ -33,25 +35,28 @@ public class EPSMeanCompareStrategy implements Strategy {
 
     @Override
     public boolean isMeet() {
+        boolean result = false;
         Table filter = this.filter();
+        Double meanEps = Double.NaN;
         if (filter.rowCount() != 0) {
-            double meanEsp = (double) filter.summarize("eps", mean).apply().get(0, 0);
-            return switch (this.operator) {
-                case GREATER -> meanEsp > compareNum;
-                case GREATER_EQUAL -> meanEsp >= compareNum;
-                case LESS -> meanEsp < compareNum;
-                case LESS_EQUAL -> meanEsp <= compareNum;
+            meanEps = (double) filter.summarize("eps", mean).apply().get(0, 0);
+            result = switch (this.operator) {
+                case GREATER -> meanEps > this.compareNum;
+                case GREATER_EQUAL -> meanEps >= this.compareNum;
+                case LESS -> meanEps < this.compareNum;
+                case LESS_EQUAL -> meanEps <= this.compareNum;
                 default -> false;
             };
         }
-        return false;
+        log.debug("meanEps = {}, compareNum = {}, operator = {}", meanEps, this.compareNum, this.operator);
+        return result;
     }
 
     private Table filter() {
         Table filter = this.epsData.emptyCopy();
         List<LocalDateTime> preDateTime = new ArrayList<>();
         for (int i = 1; i <= this.preIntervalSeason; i++) {
-            preDateTime.add(this.localDateTime.plusMonths(-3 * i));
+            preDateTime.add(this.localDateTime.plusMonths(-3L * i));
         }
         if (CollectionUtils.isEmpty(preDateTime)) return filter;
 
@@ -64,6 +69,7 @@ public class EPSMeanCompareStrategy implements Strategy {
                     .and(this.epsData.intColumn("season").isEqualTo(season)));
             filter.append(temp);
         }
+        log.debug("filter = {}", filter.print());
         return filter;
     }
 
